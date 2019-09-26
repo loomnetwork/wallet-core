3ca0566b01d3c720b682a66af7b85367b8f3f48d
Piers Shepperson <piers.shepperson@btinternet.com>
2019-09-26 10:34:55 +0100

    wip

--- src/Loom/Signer.cpp
+++ src/Loom/Signer.cpp
@@ -27,33 +27,24 @@ std::cout << "piers Signer1\n";
     const auto pubKey = privateKey.getPublicKey(TWPublicKeyTypeSECP256k1);
     signedTx.set_public_key(pubKey.bytes.data(), pubKey.bytes.size());
 
-    std::string signedBytes;
-    signedTx.SerializeToString(&signedBytes);
+    auto signedBytes = signedTx.SerializeAsString();
 std::cout << "piers Signer2\n";
     auto output = Proto::SigningOutput();
-    output.set_encoded(signedBytes);
+    output.set_encoded(signedBytes.data(), signedBytes.size());
     return output;
 }
 
 Proto::SignedTx Signer::buildTransaction(const Proto::SigningInput &input) const noexcept {
     std::cout << "piers buildTransaction0\n";
-
-    std::string data = "fred";
+    auto messageTx = Proto::MessageTx();
     if (Proto::TXType::CALL == input.id()) {
         auto callTx = Proto::CallTx();
-        std::cout << "piers buildTransaction01\n";
         callTx.set_vm_type(input.vm_type());
-        std::cout << "piers buildTransaction02\n";
-        auto value = input.value();
-        std::cout << "piers buildTransaction03\n";
-        callTx.set_allocated_value(&value);
-        std::cout << "piers buildTransaction04\n";
-        callTx.set_input(input.payload());
-        std::cout << "piers buildTransaction05\n";
-        //callTx.SerializeToString(&data);
-        //callTx.set_contract_version("dummy");
-        data = callTx.SerializeAsString();
-
+        //auto value = input.value();
+        //callTx.set_allocated_value(&value);
+        callTx.set_input(input.payload().data(), input.payload().size());
+        auto data = callTx.SerializeAsString();
+        messageTx.set_data(data.data(), data.size());
         std::cout << "piers buildTransaction06\n" ;//<< data;
     } else {
         std::cout << "piers deokiy buildTransaction07\n";
@@ -61,33 +52,31 @@ Proto::SignedTx Signer::buildTransaction(const Proto::SigningInput &input) const
         deployTx.set_vm_type(input.vm_type());
         auto value = input.value();
         deployTx.set_allocated_value(&value);
-        deployTx.set_code(input.payload());
-        deployTx.SerializeToString(&data);
+        deployTx.set_code(input.payload().data(), input.payload().size());
+        auto data = deployTx.SerializeAsString();
+        messageTx.set_data(data.data(), data.size());
     }
 std::cout << "piers buildTransaction3\n";
-    auto messageTx = Proto::MessageTx();
-    messageTx.set_data(data);
     auto to = input.to();
     messageTx.set_allocated_to(&to);
-    auto from = input.to();
+    auto from = input.from();
     messageTx.set_allocated_from(&from);
     std::string message;
-    messageTx.SerializeToString(&message);
+    auto mesgData = messageTx.SerializeAsString();
 std::cout << "piers buildTransaction§2\n";
     auto transactionTx = Proto::Transaction();
     transactionTx.set_id(input.id());
-    transactionTx.set_data(message);
-    std::string transaction;
-    transactionTx.SerializeToString(&transaction);
+    transactionTx.set_data(mesgData.data(), mesgData.size());
+    auto txData = transactionTx.SerializeAsString();
 std::cout << "piers buildTransaction4\n";
     auto nonceTx = Proto::NonceTx();
-    nonceTx.set_inner(transaction);
+    nonceTx.set_inner(txData.data(), txData.size());
     nonceTx.set_sequence(input.sequence());
     std::string nonce;
-    nonceTx.SerializeToString(&nonce);
+    auto nonceData = nonceTx.SerializeAsString();
 std::cout << "piers buildTransaction5\n";
     auto signedTx = Proto::SignedTx();
-    signedTx.set_inner(nonce);
+    signedTx.set_inner(nonceData.data(), nonceData.size());
 std::cout << "piers buildTransaction6\n";
     return signedTx;
 }
